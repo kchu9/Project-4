96d348e5c15ecbed3253d75ad11e9cff67f119ab
kchu9 <kchu9@gatech.edu>
2013-04-16 18:14:22 -0400

    added mpvfs.c

---
+++ Encryption/mpvfs.c
@@ -0,0 +1,451 @@
+/*
+  FUSE: Filesystem in Userspace
+  Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
+
+ Modified for CS 3210 by Kevin and Dennis
+
+
+
+  gcc -Wall `pkg-config fuse --cflags` mpvfs.c -o fusexmp `pkg-config fuse --libs`
+
+  Note: This implementation is largely stateless and does not maintain
+        open file handels between open and release calls (fi->fh).
+        Instead, files are opened and closed as necessary inside read(), write(),
+        etc calls. As such, the functions that rely on maintaining file handles are
+        not implmented (fgetattr(), etc). Those seeking a more efficient and
+        more complete implementation may wish to add fi->fh support to minimize
+        open() and close() calls and support fh dependent functions.
+
+*/
+
+#define FUSE_USE_VERSION 28
+#define HAVE_SETXATTR
+#define BUFSIZE 256
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef linux
+/* For pread()/pwrite() */
+#define _XOPEN_SOURCE 500
+#endif
+
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+#ifdef HAVE_SETXATTR
+#include <sys/xattr.h>
+#endif
+
+typedef struct {
+    char *rootdir;
+    char key[32];
+    char iv[32];
+} mpv_state;
+
+static char *mpv_fullpath(char *buf, const char *path, size_t bufsize){
+    leet_state *state = (leet_state *)(fuse_get_context()->private_data);
+    snprintf(buf, bufsize, "%s%s", state->rootdir, path);
+    return buf;
+}
+
+static int mpv_getattr(const char *path, struct stat *stbuf)
+{
+	int res;
+	char buff[BUFSIZE];
+	res = lstat(_leet_fullpath(buf, path, BUFSIZE), stbuf);
+	res = lstat(path, stbuf);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int mpv_access(const char *path, int mask)
+{
+	int res;
+
+	res = access(path, mask);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int mpv_readlink(const char *path, char *buf, size_t size)
+{
+	int res;
+
+	res = readlink(path, buf, size - 1);
+	if (res == -1)
+		return -errno;
+
+	buf[res] = '\0';
+	return 0;
+}
+
+
+static int mpv_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+		       off_t offset, struct fuse_file_info *fi)
+{
+	DIR *dp;
+	struct dirent *de;
+
+	(void) offset;
+	(void) fi;
+
+	dp = opendir(path);
+	if (dp == NULL)
+		return -errno;
+
+	while ((de = readdir(dp)) != NULL) {
+		struct stat st;
+		memset(&st, 0, sizeof(st));
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+		if (filler(buf, de->d_name, &st, 0))
+			break;
+	}
+
+	closedir(dp);
+	return 0;
+}
+
+static int mpv_mknod(const char *path, mode_t mode, dev_t rdev)
+{
+	int res;
+
+	/* On Linux this could just be 'mknod(path, mode, rdev)' but this
+	   is more portable */
+	if (S_ISREG(mode)) {
+		res = open(path, O_CREAT | O_EXCL | O_WRONLY, mode);
+		if (res >= 0)
+			res = close(res);
+	} else if (S_ISFIFO(mode))
+		res = mkfifo(path, mode);
+	else
+		res = mknod(path, mode, rdev);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int mpv_mkdir(const char *path, mode_t mode)
+{
+	int res;
+
+	res = mkdir(path, mode);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int mpv_unlink(const char *path)
+{
+	int res;
+
+	res = unlink(path);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int mpv_rmdir(const char *path)
+{
+	int res;
+
+	res = rmdir(path);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int mpv_symlink(const char *from, const char *to)
+{
+	int res;
+
+	res = symlink(from, to);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int mpv_rename(const char *from, const char *to)
+{
+	int res;
+
+	res = rename(from, to);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int mpv_link(const char *from, const char *to)
+{
+	int res;
+
+	res = link(from, to);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int mpv_chmod(const char *path, mode_t mode)
+{
+	int res;
+
+	res = chmod(path, mode);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int mpv_chown(const char *path, uid_t uid, gid_t gid)
+{
+	int res;
+
+	res = lchown(path, uid, gid);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int mpv_truncate(const char *path, off_t size)
+{
+	int res;
+
+	res = truncate(path, size);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int mpv_utimens(const char *path, const struct timespec ts[2])
+{
+	int res;
+	struct timeval tv[2];
+
+	tv[0].tv_sec = ts[0].tv_sec;
+	tv[0].tv_usec = ts[0].tv_nsec / 1000;
+	tv[1].tv_sec = ts[1].tv_sec;
+	tv[1].tv_usec = ts[1].tv_nsec / 1000;
+
+	res = utimes(path, tv);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int mpv_open(const char *path, struct fuse_file_info *fi)
+{
+	int res;
+
+	res = open(path, fi->flags);
+	if (res == -1)
+		return -errno;
+
+	close(res);
+	return 0;
+}
+
+static int mpv_read(const char *path, char *buf, size_t size, off_t offset,
+		    struct fuse_file_info *fi)
+{
+	int fd;
+	int res;
+
+	(void) fi;
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pread(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	return res;
+}
+
+static int mpv_write(const char *path, const char *buf, size_t size,
+		     off_t offset, struct fuse_file_info *fi)
+{
+	int fd;
+	int res;
+
+	(void) fi;
+	fd = open(path, O_WRONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pwrite(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	return res;
+}
+
+static int mpv_statfs(const char *path, struct statvfs *stbuf)
+{
+	int res;
+
+	res = statvfs(path, stbuf);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int mpv_create(const char* path, mode_t mode, struct fuse_file_info* fi) {
+
+    (void) fi;
+
+    int res;
+    res = creat(path, mode);
+    if(res == -1)
+	return -errno;
+
+    close(res);
+
+    return 0;
+}
+
+
+static int mpv_release(const char *path, struct fuse_file_info *fi)
+{
+	/* Just a stub.	 This method is optional and can safely be left
+	   unimplemented */
+
+	(void) path;
+	(void) fi;
+	return 0;
+}
+
+static int mpv_fsync(const char *path, int isdatasync,
+		     struct fuse_file_info *fi)
+{
+	/* Just a stub.	 This method is optional and can safely be left
+	   unimplemented */
+
+	(void) path;
+	(void) isdatasync;
+	(void) fi;
+	return 0;
+}
+
+#ifdef HAVE_SETXATTR
+static int mpv_setxattr(const char *path, const char *name, const char *value,
+			size_t size, int flags)
+{
+	int res = lsetxattr(path, name, value, size, flags);
+	if (res == -1)
+		return -errno;
+	return 0;
+}
+
+static int mpv_getxattr(const char *path, const char *name, char *value,
+			size_t size)
+{
+	int res = lgetxattr(path, name, value, size);
+	if (res == -1)
+		return -errno;
+	return res;
+}
+
+static int mpv_listxattr(const char *path, char *list, size_t size)
+{
+	int res = llistxattr(path, list, size);
+	if (res == -1)
+		return -errno;
+	return res;
+}
+
+static int mpv_removexattr(const char *path, const char *name)
+{
+	int res = lremovexattr(path, name);
+	if (res == -1)
+		return -errno;
+	return 0;
+}
+#endif /* HAVE_SETXATTR */
+
+static struct fuse_operations mpv_oper = {
+	.getattr	= mpv_getattr,
+	.access		= mpv_access,
+	.readlink	= mpv_readlink,
+	.readdir	= mpv_readdir,
+	.mknod		= mpv_mknod,
+	.mkdir		= mpv_mkdir,
+	.symlink	= mpv_symlink,
+	.unlink		= mpv_unlink,
+	.rmdir		= mpv_rmdir,
+	.rename		= mpv_rename,
+	.link		= mpv_link,
+	.chmod		= mpv_chmod,
+	.chown		= mpv_chown,
+	.truncate	= mpv_truncate,
+	.utimens	= mpv_utimens,
+	.open		= mpv_open,
+	.read		= mpv_read,
+	.write		= mpv_write,
+	.statfs		= mpv_statfs,
+	.create         = mpv_create,
+	.release	= mpv_release,
+	.fsync		= mpv_fsync,
+#ifdef HAVE_SETXATTR
+	.setxattr	= mpv_setxattr,
+	.getxattr	= mpv_getxattr,
+	.listxattr	= mpv_listxattr,
+	.removexattr	= mpv_removexattr,
+#endif
+};
+
+int main(int argc, char *argv[])
+{
+    umask(0);
+    mpv_state state;
+
+    if(argc < 4){
+#ifdef SECURE_KEYPHRASE
+        if(argc == 3){
+            /* TODO: implement a secure key phrase input technique */
+
+        }else{
+            /* TODO: Fail or implement defaults */
+
+        }
+#endif
+        fprintf(stderr, "leetfs usage: ./pa5-encfs %s %s %s\n",
+                "<Key Phrase>", "<Mirror Directory>", "<Mount Point>");
+        return 1;
+    }
+
+    state.rootdir = realpath(argv[2], NULL);
+    strncpy(state.key, argv[1], 32);
+    state.key[31] = '\0';
+    
+    return fuse_main(argc - 2, argv + 2, &mpv_oper, &state);
+}
+/*int main(int argc, char *argv[])
+{
+	umask(0);
+	return fuse_main(argc, argv, &mpv_oper, NULL);
+}*/
