849dde59ccfd137f8171875856e1ba06758b097c
kchu9 <kchu9@gatech.edu>
2013-04-18 18:36:38 -0400

    backtrack

--- Encryption/pr4fs.c
+++ Encryption/pr4fs.c
@@ -154,9 +154,10 @@ static int mpv_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	while ((de = readdir(dp)) != NULL) {
 		struct stat st;
 		memset(&st, 0, sizeof(st));
-		//try strncat();
-		//snprintf(tempPath,BUFSIZE*2,"%s%s",path,de->d_name); 
-		//lstat(mpv_fullpath(buf, tempPath, 2*BUFSIZE), &st);
+		//tempPath=pathbuf;
+		//strcat(tempPath,de->d_name);
+		//lstat(tempPath,&st);
+		printf("Name: %s; uid: %d",de->d_name,st->st_uid);
 		st.st_ino = de->d_ino;
 		st.st_mode = de->d_type << 12;
 		if (filler(buf, de->d_name, &st, 0))
