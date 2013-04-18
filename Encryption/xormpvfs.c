ce9201a131498ec14fc6e8b2d0f13281419ba4cd
kchu9 <kchu9@gatech.edu>
2013-04-18 00:12:12 -0400

    xormpvfs update

--- Encryption/xormpvfs.c
+++ Encryption/xormpvfs.c
@@ -570,10 +570,10 @@ static int mpv_create(const char* path, mode_t mode, struct fuse_file_info* fi) 
 	   // mpv_state *state = (mpv_state *)(fuse_get_context()->private_data);
 	//  xor_do_crypt(res, AES_ENCRYPT, state->key);
 
-	    if(fsetxattr(fileno(res), ENCRYPTED_ATTR, "true", strlen("true"), 0)){
+	    if(setxattr(buf, ENCRYPTED_ATTR, "true", strlen("true"), 0)){
 		return -errno;
 	    }
-	if(fsetxattr(fileno(res), IS_ENCRYPTED, "false", strlen("false"), 0)){
+	if(setxattr(buf, IS_ENCRYPTED, "false", strlen("false"), 0)){
 		return -errno;
 	    }
 	char *tmpval;
