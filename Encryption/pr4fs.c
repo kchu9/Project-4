3d88d814f7d85600d8c5e442d7333f28e24a5fed
kchu9 <kchu9@gatech.edu>
2013-04-18 22:03:50 -0300

    Basic encryption and filtering should work
    Basic encryption-> doesn't work with externally created public files
    Basic filtering-> only check filer user owner and current uid

--- Encryption/pr4fs.c
+++ Encryption/pr4fs.c
@@ -163,12 +163,8 @@ static int mpv_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 		if(getuid()==st.st_uid)
 		{
 			if (filler(buf, de->d_name, &st, 0))
-<<<<<<< HEAD
 			{	
 				break;
-=======
-			{break;
->>>>>>> bf7c68dff6de3aedcf9461d5d7941e14bf31eb18
 			}
 		}
 	}
@@ -691,4 +687,3 @@ int main(int argc, char *argv[])
 	umask(0);
 	return fuse_main(argc, argv, &mpv_oper, NULL);
 }*/
-
